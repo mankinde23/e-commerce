5da1dc97b3d5d671657f72a5a596a00713c6ccb9
mankinde23 <makinded12@gmail.com>
2023-12-03 15:38:44 +0000

    at this stage i have an api that as an array of data containing what i want to use to create the e-commerce application

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 const Home = () => {
+    const [data, setData] = useState(null)
+    const fetchData = async () => {
+        try {
+            const resp = await axios.get('https://fakestoreapi.com/products')
+            setData(resp.data)
+            console.log(data)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     return (
         <div>
+
 
         </div>
     )
